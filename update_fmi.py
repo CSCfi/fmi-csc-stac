8056a105a4a3371cefa3981f019ec688af6bde09
ehuusko <ehuusko@csc.fi>
2023-04-04 14:44:51 +0300

    Collection additions and update script

--- update_fmi.py
+++ update_fmi.py
@@ -1,11 +1,13 @@
 import json
+import sys
+import argparse
 import getpass
-from pathlib import Path
 import requests
 import pystac_client
 from requests.auth import HTTPBasicAuth
 from urllib.parse import urljoin
-from pystac import Catalog, Collection
+from pystac import Collection
+import pandas as pd
 import pystac
 import rasterio
 import urllib.request, json
@@ -18,7 +20,7 @@ fmi_collections = [
     "https://pta.data.lit.fmi.fi/stac/catalog/Sentinel-1_dekadi_mosaiikki/Sentinel-1_dekadi_mosaiikki.json",
     "https://pta.data.lit.fmi.fi/stac/catalog/Sentinel-1_daily_mosaiikki/Sentinel-1_daily_mosaiikki.json",
     "https://pta.data.lit.fmi.fi/stac/catalog/Sentinel-1_osakuvat/Sentinel-1_osakuvat.json",
-    #"https://pta.data.lit.fmi.fi/stac/catalog/Landsat_pintaheijastus/Landsat_pintaheijastus.json", # THE CHILDREN ARE NOT RIGHT
+    #"https://pta.data.lit.fmi.fi/stac/catalog/Landsat_pintaheijastus/Landsat_pintaheijastus.json", # THE CHILDREN ARE NOT CORRECT
     "https://pta.data.lit.fmi.fi/stac/catalog/Landsat_indeksit/Landsat_indeksit.json",
     "https://pta.data.lit.fmi.fi/stac/catalog/Metsavarateema/Metsavarateema.json",
     "https://pta.data.lit.fmi.fi/stac/catalog/Latvuskorkeusmalli/Latvuskorkeusmalli.json",
@@ -26,61 +28,6 @@ fmi_collections = [
     "https://pta.data.lit.fmi.fi/stac/catalog/Myrskytuhoriskikartta/Myrskytuhoriskikartta.json",
     "https://pta.data.lit.fmi.fi/stac/catalog/Tuulituhoriski/Tuulituhoriski.json"
 ]
-
-collection_info = {
-    "sentinel_2_annual_mosaics_at_fmi" : {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/sentinel-2-satellite-image-mosaics-s2gm-sentinel-2-satelliittikuvamosaiikki-s2gm",
-    },
-    "sentinel_2_11_days_mosiacs_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/sentinel-2-satellite-image-mosaics-s2gm-sentinel-2-satelliittikuvamosaiikki-s2gm",
-    },
-    "sentinel_2_monthly_index_mosaics_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/sentinel-2-image-index-mosaics-s2ind-sentinel-2-kuvamosaiikit-s2ind",
-    },
-    "sentinel_1_11_days_mosaics_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/sentinel-1-sar-image-mosaic-s1sar-sentinel-1-sar-kuvamosaiikki-s1sar",
-    },
-    "sentinel_1_daily_mosaics_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/sentinel-1-sar-image-mosaic-s1sar-sentinel-1-sar-kuvamosaiikki-s1sar",
-    },
-    "sentinel_1_tiles_at_fmi": {
-        "licenseURL":
-            "https://sentinels.copernicus.eu/documents/247904/690755/Sentinel_Data_Legal_Notice",
-    },
-    "landsat_yearly_mosaics_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/historical-landsat-satellite-image-mosaics-href-historialliset-landsat-kuvamosaiikit-href",
-    },
-    "landsat_annual_index_mosaics_at_fmi": {
-        "licenseURL":
-            "https://ckan.ymparisto.fi/dataset/historical-landsat-image-index-mosaics-hind-historialliset-landsat-kuvaindeksimosaiikit-hind",
-    },
-    "forest_inventory_at_fmi": {
-        "licenseURL":
-            None,
-    },
-    "canopy_height_model_at_fmi": {
-        "licenseURL":
-            None,
-    },
-    "2m_digital_terrain_model_products_at_fmi": {
-        "licenseURL":
-            "https://www.maanmittauslaitos.fi/en/opendata-licence-cc40",
-    },
-    "forest_wind_damage_risk_at_fmi": {
-        "licenseURL":
-            None
-    },
-    "daily_wind_damage_risk_at_fmi": {
-        "licenseURL":
-            None
-    }
-}
 
 news_ids = {
     "Sentinel-2_global_mosaic_vuosi": "sentinel_2_annual_mosaics_at_fmi",
@@ -98,30 +45,32 @@ news_ids = {
     "Tuulituhoriski": "daily_wind_damage_risk_at_fmi"
 }
 
-
 def retry_errors(list_of_items, list_of_errors):
+    """
+    Function to retry retrieving the items that were timed out during the process.
+    
+    list_of_items - List containing the STAC items from the source. The errored items will be appended to this list when successfully retrieved
+    list_of_errors - List of links to the items that timed out during the retrieving process. Function will run until this list is empty
+    """
 
-    print("TRYING TO ADD ERRORED ITEMS")
+    print(" * Trying to add items that timedout")
     while len(list_of_errors) > 0:
         for i,item in enumerate(list_of_errors):
             try:
                 list_of_items.append(pystac.Item.from_file(item))
-                print(f"Added item {item}")
+                print(f" * Added item {item}")
                 list_of_errors.remove(item)
             except Exception as e:
-                print(f"ERROR {e} in item {item} #{i}")
-
-    return 0
-
+                print(f" ! Timeout on {item} #{i}")
 
 def json_convert(content):
 
-    """
-        jsonfile: json file in dict format
-        
-        A function to map the Sentinel-2 STAC jsonfiles into the GeoServer database layout.
-        There are different json layouts for Collections and Items. The function checks if the jsonfile is of type "Collection",
-        or of type "Feature" (=Item). A number of properties are hardcoded into Sentinel-2 metadata as these are not collected in the STAC jsonfiles.
+    """ 
+    A function to map the Sentinel-2 STAC dictionaries into the GeoServer database layout.
+    There are different json layouts for Collections and Items. The function checks if the dictionary is of type "Collection",
+    or of type "Feature" (=Item).
+
+    content - STAC dictionary from where the modified JSON will be made
     """
     
     if content["type"] == "Collection":
@@ -169,7 +118,7 @@ def json_convert(content):
                 "derivedFrom": {
                     "href": content["derived_from"],
                     "rel": "derived_from",
-                    "type": "application/geo+json"
+                    "type": "application/json"
                 }, # Derived_from added
                 "queryables": [
                     "eo:identifier"
@@ -182,7 +131,11 @@ def json_convert(content):
 
         for link in content["links"]:
             if link["rel"] == "license":
-                new_json["properties"]["licenseURL"] = link["href"]
+                new_json["properties"]["licenseLink"] = {
+                    "href": link["href"],
+                    "rel": "license",
+                    "type": "application/json"
+                } # New License URL link
 
     if content["type"] == "Feature":
 
@@ -209,7 +162,14 @@ def json_convert(content):
 
     return new_json
 
-def create_catalog(app_host, csc_catalog):
+def update_catalog(app_host, csc_catalog_client):
+
+    """
+    The main updating function of the script. Checks the collection items in the FMI catalog and compares the to the ones in CSC catalog.
+
+    app_host - The REST API path for updating the collections
+    csc_catalog_client - The STAC API path for checking which items are already in the collections
+    """
 
     collections = []
     for collection in fmi_collections:
@@ -224,7 +184,7 @@ def create_catalog(app_host, csc_catalog):
     for collection in collections:
 
         collection.id = news_ids[collection.id]
-        print(f"Checking collection {collection.id}")
+        print(f"# Checking collection {collection.id}:")
         collection_links = collection.get_child_links()
 
         sub_collections = []
@@ -239,7 +199,7 @@ def create_catalog(app_host, csc_catalog):
 
         item_links = list(set([link.target for sub in sub_collections for link in sub.get_item_links()]))
 
-        csc_collection = csc_catalog.get_collection(collection.id)
+        csc_collection = csc_catalog_client.get_collection(collection.id)
         csc_item_ids = [x.id for x in csc_collection.get_items()]
 
         items = []
@@ -248,19 +208,18 @@ def create_catalog(app_host, csc_catalog):
             try:
                 items.append(pystac.Item.from_file(item))
             except Exception as e:
-                print(f"ERROR {e} in item {item} #{i}")
+                print(f" ! Timeout on {item} #{i}")
                 errors.append(item)
-        print(f" * Number of items in CSC STAC vs FMI for {collection.id}: {len(csc_item_ids)}/{len(items)}")
 
         # If there were connection errors during the item making process, the item generation for errors is retried
         if len(errors) > 0:
             retry_errors(items, errors)
-            print("All errors fixed")
+            print(" * All errors fixed")
+
+        print(f" * Number of items in CSC STAC and FMI for {collection.id}: {len(csc_item_ids)}/{len(items)}")
 
         for i,item in enumerate(items):
-
             if item.id not in csc_item_ids:
-
                 collection.add_item(item)
 
                 with rasterio.open(next(iter(item.assets.values())).href) as src:
@@ -284,15 +243,8 @@ def create_catalog(app_host, csc_catalog):
     
                 del item.extra_fields["license"]
                 item.remove_links("license")
-                # if collection_info[collection.id]["licenseURL"]:
-                #     item.add_link(pystac.Link(
-                #         rel="license",
-                #         target=collection_info[collection.id]["licenseURL"],
-                #         title="License"
-                #     ))
 
                 item_dict = item.to_dict()
-
                 converted = json_convert(item_dict)
 
                 request_point = f"collections/{collection.id}/products"
@@ -302,12 +254,32 @@ def create_catalog(app_host, csc_catalog):
                 print(f" + Added item {item.id} to {collection.id}")
 
         print(f" * All items present in {collection.id}")
-
+    
 if __name__ == "__main__":
 
-    pwd = getpass.getpass()
+    """
+    The first check for REST API password is a --pwd argument. If there is none provided, second check is from a password file. 
+    If a password file is not found, the script prompts the user to give a password through CLI
+    """
+    pw_filename = 'passwords.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, help="Hostname of the selected STAC API", required=True)
+    parser.add_argument("--pwd", type=str, help="Password for the API")
+    
+    args = parser.parse_args()
 
-    app_host = "http://86.50.229.158:8080/geoserver/rest/oseo/"
-    csc_catalog = pystac_client.Client.open("http://86.50.229.158:8080/geoserver/ogc/stac/")
+    if args.pwd:
+        pwd = args.pwd
+    else:
+        try:
+            pw_file = pd.read_csv(pw_filename, header=None)
+            pwd = pw_file.at[0,0]
+        except FileNotFoundError:
+            print("Password not given as an argument and no password file found")
+            pwd = getpass.getpass()
 
-    create_catalog(app_host, csc_catalog)
+    app_host = f"{args.host}/geoserver/rest/oseo/"
+    csc_catalog_client = pystac_client.Client.open(f"{args.host}/geoserver/ogc/stac/")
+
+    print(f"Updating STAC Catalog at {args.host}")
+    update_catalog(app_host, csc_catalog_client)
