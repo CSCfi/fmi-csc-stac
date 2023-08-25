d9980a14eb684ee58897f7a29d46ab9383ec42fe
ehuusko <ehuusko@csc.fi>
2023-08-25 10:07:43 +0300

    Fixed API endpoint

--- fmi_to_geoserver.py
+++ fmi_to_geoserver.py
@@ -124,7 +124,7 @@ if __name__ == "__main__":
     sentinel = workingdir / "FMI" / collection_name
 
     app_host = "https://paituli.csc.fi/geoserver/rest/oseo/"
-    catalog = pystac_client.Client.open("https://paituli.csc.fi/geoserver/ogc/stac/", request_modifier=change_to_https)
+    catalog = pystac_client.Client.open("https://paituli.csc.fi/geoserver/ogc/stac/v1/", request_modifier=change_to_https)
 
     # Convert the STAC collection json into json that GeoServer can handle
     converted = json_convert(sentinel / "collection.json")
